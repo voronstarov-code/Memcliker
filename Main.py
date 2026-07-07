fcaf67e49635ca076d2d7c7507e7c57a2c2242c9
voronstarov-code <voronstarov@gmail.com>
2026-07-07 11:11:19 +0500

    Main.py

--- Main.py
+++ Main.py
@@ -2,10 +2,25 @@ import kivy
 kivy.require('2.1.0')
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+import random
 
-class TestApp(App):
+class MainApp(App):
     def build(self):
-        return Button(text='Hello, World!')
+        self.coins = 0
+        self.layout = BoxLayout(orientation='vertical')
+        self.label = Label(text="0")
+        btn = Button(text="Клик")
+        btn.bind(on_press=self.click)
+        self.layout.add_widget(btn)
+        self.layout.add_widget(self.label)
+        return self.layout
+    
+    def click(self, instance):
+        self.coins += 1
+        self.label.text = str(self.coins)
 
-if __name__ == '__main__':
-    TestApp().run()
+if __name__ == "__main__":
+    MainApp().run()
